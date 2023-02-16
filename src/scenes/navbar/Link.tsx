8c01dd85ffaee69fce389cc163bc44fe62434725
haixin7 <haixin.wang7@gmail.com>
2023-02-16 00:45:41 -0500

    first commit

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,28 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "@/shared/types";
+
+type Props = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+  //doing it for using it for id
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage; //remove space
+  //ts can not know we convert it. use "as" SelectedPage force it, tell ts treat lowercasepage as SelectedPage type
+
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-primary-500" : ""
+      } transition duration-500 hover:text-primary-300`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+export default Link;
---
+++ src/shared/types.ts
@@ -0,0 +1,19 @@
+//enum does exist in the actual buid in the run time (browser)
+export enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUs = "contactus",
+}
+
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
